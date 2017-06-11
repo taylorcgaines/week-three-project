ee885c9b13ea72557ec3f9227fe9eb5ca4b16579
Taylor Gaines <taylorcgaines@gmail.com>
2017-06-10 23:01:03 -0500

    IT WORKS

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 
 var calcequation = ""
-
 // sets the name of .calcfield to calcfield
 var calcfield = document.querySelector(".calcfield")
 
@@ -15,16 +14,13 @@ for (i = 0; i<buttonList.length; i++){
 }
 
 function buttonPress(event){
-  if(event.target.textContent==='c'){
-// this will save over calcequation with an empty string
+  if(event.target.textContent=='c'){
     calcequation = ""
     calcfield.textContent = calcequation
-  }else if(event.target.textContent==='='){
-// this will use eval on calcequation and save it to calcequation
+  }else if(event.target.textContent=='='){
     calcequation = eval(calcequation)
     calcfield.textContent = calcequation
   }else{
-// this will get the textcontent of the buttons and send it to calcequation
     calcequation = calcequation + event.target.textContent
     calcfield.textContent = calcequation;
   }
