6afe2904ef077400f864b5adae6a98a2aea194a5
Taylor Gaines <taylorcgaines@gmail.com>
2017-06-10 20:35:14 -0500

    button push working

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 
-var calcequation = "test"
+var calcequation = ""
 
 // sets the name of .calcfield to calcfield
 var calcfield = document.querySelector(".calcfield")
 
+calcfield.textContent = calcequation;
+
 // sets the name of all buttons with class .calcbutt to button
-var button = document.querySelectorAll(".calcbutt")
+var buttonList = document.querySelectorAll(".calcbutt")
+
+for (i = 0; i<buttonList.length; i++){
+  buttonList[i].addEventListener('click',buttonPress)
+}
 
 // function to get the textcontent of the buttons and send it to calcequation
-button.addEventListener('click', function buttonPress(event) {
+function buttonPress(event){
+if ((event.target.textContent!=='c')||(event.target.textContent!=='=')){
   calcequation = calcequation + event.target.textContent
   calcfield.textContent = calcequation;
   return event;
-}
+}}
