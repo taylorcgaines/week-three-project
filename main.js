55bebdd4f0596b680a8c463610e48c3710faf90f
Taylor Gaines <taylorcgaines@gmail.com>
2017-06-10 22:04:21 -0500

    This should have made = and c work, but didn't

--- main.js
+++ main.js
@@ -9,14 +9,24 @@ calcfield.textContent = calcequation;
 // sets the name of all buttons with class .calcbutt to button
 var buttonList = document.querySelectorAll(".calcbutt")
 
+// adds a listener to each button that waits for a click
 for (i = 0; i<buttonList.length; i++){
   buttonList[i].addEventListener('click',buttonPress)
 }
 
-// function to get the textcontent of the buttons and send it to calcequation
 function buttonPress(event){
-if ((event.target.textContent!=='c')||(event.target.textContent!=='=')){
-  calcequation = calcequation + event.target.textContent
-  calcfield.textContent = calcequation;
+  if(event.target.textContent==='c'){
+// this will save over calcequation with an empty string
+    calcequation = ""
+    calcfield.textContent = calcequation
+  }else if(event.target.textContent==='='){
+// this will use eval on calcequation and save it to calcequation
+    calcequation = eval(calcequation)
+    calcfield.textContent = calcequation
+  }else{
+// this will get the textcontent of the buttons and send it to calcequation
+    calcequation = calcequation + event.target.textContent
+    calcfield.textContent = calcequation;
+  }
   return event;
-}}
+}
